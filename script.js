5ab3517195f9dcc1cd5bb9ff4b24f28c2d4bd10e
A. S. Koepke <a-sophia.koepke@uni-tuebingen.de>
2024-12-02 20:15:21 +0100

    style update and added CVPRW paper

--- script.js
+++ script.js
@@ -1,44 +1,40 @@
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav-links');
-const links = navLinks.querySelectorAll('a'); // Select all links in the nav
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+const links = navLinks.querySelectorAll("a"); // Select all links in the nav
 
-hamburger.addEventListener('click', function() {
-    navLinks.classList.toggle('active'); // Toggle active class
+hamburger.addEventListener("click", function () {
+  navLinks.classList.toggle("active"); // Toggle active class
 });
 
 // Collapse the nav when a link is clicked
-links.forEach(link => {
-    link.addEventListener('click', function() {
-        navLinks.classList.remove('active'); // Remove active class
-    });
+links.forEach((link) => {
+  link.addEventListener("click", function () {
+    navLinks.classList.remove("active"); // Remove active class
+  });
 });
 
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    const targetId = this.getAttribute("href");
+    e.preventDefault();
 
-document.querySelectorAll('a[href^="#"], a[href="index.html"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        const targetId = this.getAttribute('href');
+    if (targetId === "#") {
+      // Scroll to the top smoothly
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    } else if (targetId.startsWith("#")) {
+      const targetElement = document.querySelector(targetId);
+      if (targetElement) {
+        const navbarHeight = 70; // Adjust to your navbar height
+        const targetPosition = targetElement.getBoundingClientRect().top + window.scrollY - navbarHeight;
 
-        // Prevent default behavior for links
-        e.preventDefault();
-
-        if (targetId === "index.html") {
-            // Scroll to the top smoothly
-            window.scrollTo({
-                top: 0,
-                behavior: 'smooth'
-            });
-        } else if (targetId.startsWith("#")) {
-            const targetElement = document.querySelector(targetId);
-            if (targetElement) {
-                const navbarHeight = 70; // Adjust to your navbar height
-                const targetPosition = targetElement.getBoundingClientRect().top + window.scrollY - navbarHeight;
-
-                window.scrollTo({
-                    top: targetPosition,
-                    behavior: 'smooth'
-                });
-            }
-        }
-    });
+        window.scrollTo({
+          top: targetPosition,
+          behavior: "smooth",
+        });
+      }
+    }
+  });
 });
-
